1e1adbd844676fb4d71fec5c0ad460cfb1777b9f
Michael <michael_kirsch>
2020-01-25 19:53:43 +0100

    Revert "font path"
    This reverts commit 04fb8815

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,16 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
-#include <string>
-#include <experimental/filesystem>
 #include <chrono>
 int main() {
     sf::RenderWindow window;
-    window.create(sf::VideoMode(800,600),"SearchCam");
+    window.create(sf::VideoMode::getDesktopMode(),"SearchCam",sf::Style::Fullscreen);
     window.setMouseCursorVisible(false);
-    sf::Text text;
-    sf::Font font;
-    //ffff
-    std::string path_exec = std::experimental::filesystem::current_path().string();
-    std::cout << path_exec << std::endl;
-    auto path_font = path_exec + "/fonts/JetBrainsMono-Regular.ttf";
-    font.loadFromFile(path_font);
-    text.setCharacterSize(0.05f*window.getSize().y);
-    text.setFillColor(sf::Color::White);
-    text.setPosition(0,0);
-    text.setString("Mode: Life-Search");
-    text.setFont(font);
     sf::RectangleShape rect;
     rect.setSize({50,50});
     rect.setPosition({50,50});
     rect.setFillColor({200,0,0});
     std::vector<sf::RectangleShape> rects;
-    //hier
+
     float tilesize_x = window.getSize().x/32;
     float tilesize_y = window.getSize().y/24;
 
@@ -40,15 +26,15 @@ int main() {
         }
 
     }
+
+
+
+    //fhafw
     sf::Clock cl;
     float timer =0;
     window.requestFocus();
     while(window.isOpen())
     {
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-        {
-            window.close();
-        }
         timer+=cl.restart().asSeconds();
         if(timer>1.f/60.f)
         {
@@ -58,7 +44,6 @@ int main() {
             {
                 window.draw(el);
             }
-            window.draw(text);
             window.display();
         }
         sf::Event ev;
@@ -69,6 +54,8 @@ int main() {
                 window.close();
             }
         }
+
+
     }
     return 0;
 }
