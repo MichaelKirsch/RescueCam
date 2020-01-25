836c605ea23ba32cc745da540d364a469a05cd12
Michael <michael_kirsch>
2020-01-25 20:26:36 +0100

    switch to vertex array

--- src/main.cpp
+++ src/main.cpp
@@ -13,19 +13,25 @@ int main() {
 
     float tilesize_x = window.getSize().x/32;
     float tilesize_y = window.getSize().y/24;
-
+    sf::VertexArray cameraView(sf::Quads,32*24*4);
+    int counter =0;
     for(int x =0;x<32;x++)
     {
         for(int y = 0;y<24;y++)
         {
-            sf::RectangleShape buffer;
-            buffer.setSize({tilesize_x,tilesize_y});
-            buffer.setPosition(x*tilesize_x,y*tilesize_y);
-            buffer.setFillColor({(sf::Uint8)(rand()%255),(sf::Uint8)(rand()%255),(sf::Uint8)(rand()%255)});
-            rects.push_back(buffer);
+            int pos = ((x*24)+y)*4;
+            cameraView[pos].position = sf::Vector2f(x*tilesize_x, y*tilesize_y);
+            cameraView[pos+1].position = sf::Vector2f((x+1)*tilesize_x, y*tilesize_y);
+            cameraView[pos+2].position = sf::Vector2f((x+1)*tilesize_x, (y+1)*tilesize_y);
+            cameraView[pos+3].position = sf::Vector2f(x*tilesize_x, (y+1)*tilesize_y);
+            sf::Color bucol = {(sf::Uint8)(rand()%255),(sf::Uint8)(rand()%255),(sf::Uint8)(rand()%255)};
+            cameraView[pos].color = bucol;
+            cameraView[pos+1].color = bucol;
+            cameraView[pos+2].color = bucol;
+            cameraView[pos+3].color = bucol;
         }
-
     }
+
     sf::Clock cl;
     float timer =0;
     window.requestFocus();
@@ -36,10 +42,7 @@ int main() {
         {
             timer = 0.f;
             window.clear();
-            for(auto el:rects)
-            {
-                window.draw(el);
-            }
+            window.draw(cameraView);
             window.display();
         }
         sf::Event ev;
