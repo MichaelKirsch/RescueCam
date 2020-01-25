326e28f7522a0f71546c5da7b8bce0517aec0cad
Michael <michael_kirsch>
2020-01-25 19:49:21 +0100

    aufl

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include <chrono>
 int main() {
     sf::RenderWindow window;
-    window.create(sf::VideoMode::getDesktopMode(),"SearchCam");
+    window.create(sf::VideoMode(800,600),"SearchCam");
     window.setMouseCursorVisible(false);
     sf::Text text;
     sf::Font font;
