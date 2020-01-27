5263e43e92675884abd81d4faffaedc69d028992
Michael <michael_kirsch>
2020-01-27 14:45:17 +0100

    thermal begin

--- src/ThermalCamera.cpp
+++ src/ThermalCamera.cpp
@@ -14,3 +14,14 @@ int8_t ThermalCamera::readRegister8(int reg) {
 int16_t ThermalCamera::readRegister16(int reg) {
     return wiringPiI2CReadReg16(adress,  reg) ;
 }
+
+std::string ThermalCamera::getBits(int data) {
+    int length_in_bits = sizeof(data)*8;
+    std::string buf;
+    for(int x = 0;x<length_in_bits;x++)
+    {
+        buf+=std::to_string(getBit(data,x));
+        buf+="|";
+    }
+    return buf;
+}
--- src/ThermalCamera.h
+++ src/ThermalCamera.h
@@ -1,15 +1,23 @@
 #pragma once
 
 #include "wiringPiI2C.h"
+#include <string>
 
 class ThermalCamera {
 public:
     ThermalCamera();
     int8_t readRegister8(int reg);
     int16_t readRegister16(int reg);
+    std::string getBits(int data);
+
+
     ~ThermalCamera()= default;
 
 private:
+    bool getBit(int data, int position) // position in range 0-7
+    {
+        return (data >> position) & 0x1;
+    }
     const int adress = 0x33;
 };
 
