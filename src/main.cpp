44035972b99e356d877c4c94960a02309d0bbba8
Michael <michael_kirsch>
2020-01-27 17:40:30 +0100

    i2c stuffy

--- src/main.cpp
+++ src/main.cpp
@@ -112,7 +112,7 @@ int main() {
             if(!camera.I2C_success())
                 text.setString("scheis i2c");
             else
-                text.setString(camera.convertToString(camera.readRegister16(0x8000)));
+                text.setString(camera.convertToString(camera.readRegister16(0x800D)));
             timer = 0.f;
             changeMode(mode,cameraView,tilesize_x,tilesize_y);
             window.clear();
