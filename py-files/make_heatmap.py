06a2ed8144ae3c23ca87308fc332b7c87b6e29dc
Michael <michael_kirsch>
2020-01-27 18:38:10 +0100

    start script

--- py-files/make_heatmap.py
+++ py-files/make_heatmap.py
@@ -8,18 +8,18 @@ i2c = busio.I2C(board.SCL, board.SDA, frequency=800000)
 mlx = adafruit_mlx90640.MLX90640(i2c)
 print("MLX addr detected on I2C", [hex(i) for i in mlx.serial_number])
 
-mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_32_HZ
+mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_8_HZ
 
 frame = [0] * 768
 while True:
     try:
         mlx.getFrame(frame)
-        filename = "newfile.txt"
-        myfile = open(filename, 'w')
-        myfile.write(frame)
-        myfile.close()
+
     except ValueError:
         continue
 
 
-
+    filename = "newfile.txt"
+    myfile = open(filename, 'w')
+    myfile.write(frame)
+    myfile.close()
