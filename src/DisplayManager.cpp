237b861fe7352bab3e1c757ae643a80fb86e82a9
Michael <michael_kirsch@t-online.de>
2020-06-03 20:24:52 +0200

    openup framrate

--- src/DisplayManager.cpp
+++ src/DisplayManager.cpp
@@ -20,7 +20,7 @@ DisplayManager::DisplayManager():camera(0x33,ThermalCamera::REFRESH_RATE::HZ_16)
     m_window.setMouseCursorVisible(false);
     cameraImage.create(640,480);
     thermalImage.create(32,24);
-
+    cameraImage.saveToFile("data/image.jpg");
     picture.setSize({(float)m_window.getSize().x,(float)m_window.getSize().y});
     end_texture.loadFromFile("data/image.jpg");
     picture.setTexture(&end_texture);
